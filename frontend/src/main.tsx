c83ddbebcd5526356cca318292eaa639ae958e9b
Mau <alan.fernandez@ucb.edu.bo>
2026-05-22 16:27:40 -0400

    configure routes for navigate into the web app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+
+
+// Componentes Dummy/Simulados (Reemplázalos por tus páginas reales)
+const GeneralDashboard = () => <div>Contenido Dashboard General</div>;
+const EstadisticasDashboard = () => <div>Contenido Estadísticas Dashboard</div>;
+
+const TablaAmenazas = () => <div>Componente Tabla de Amenazas</div>;
+const TablaControles = () => <div>Componente Tabla de Controles</div>;
+const TablaActivos = () => <div>Componente Tabla de Activos de Información</div>;
+const TablaAnalisRiesgo = () => <div>Componente Tabla de Análisis de Riesgos</div>;
+
+const MapasCalor = () => <div>Contenido de Mapas de Calor</div>;
+const Configuracion = () => <div>Contenido de Configuración</div>;
+
+import { router } from './routes/router';
+// Estructura de Navegación
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
+
